5efa3128154d94d3615df471b07b8026912f5144
Prakhar Kumar <hacker.eth@gmail.com>
2015-06-07 16:21:35 +0530

    Link the model, view and controller together

--- controller.go
+++ controller.go
@@ -3,7 +3,13 @@ package framework
 import (
 	"fmt"
 	"net/http"
+
+	"kodeclan.com/framework/utils"
 )
+
+type ControllerChild interface {
+	SetParent(*BaseController)
+}
 
 type Controller interface {
 	registerRequestAndResponse(*http.ResponseWriter, **http.Request)
@@ -12,11 +18,21 @@ type Controller interface {
 }
 
 type BaseController struct {
+	module string
+	index  string
+
 	model *BaseModel
 	view  *BaseView
 
 	request  *http.Request
 	response http.ResponseWriter
+}
+
+func (c *BaseController) SetModelAndView(m Model, v View) {
+	m.SetParent(c)
+	v.SetParent(c)
+
+	v.setPackageName(utils.GetCallingPackageName())
 }
 
 func (c *BaseController) registerRequestAndResponse(w *http.ResponseWriter, r **http.Request) {
--- view.go
+++ view.go
@@ -4,7 +4,21 @@ import (
 	"bytes"
 )
 
+type View interface {
+	ControllerChild
+	setPackageName(string)
+}
+
 type BaseView struct {
 	base   string
 	output bytes.Buffer
+}
+
+func (v *BaseView) SetParent(c *BaseController) {
+	c.view = v
+}
+
+func (v *BaseView) setPackageName(base string) {
+	v.base = base
+	checkAndParsePackageTemplates(base)
 }
