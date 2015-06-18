ba0667330a92ca8241f8e7fa590418c60458a1f4
Prakhar Kumar <hacker.eth@gmail.com>
2015-06-18 14:25:44 +0530

    Document the base model

--- model.go
+++ model.go
@@ -1,12 +1,15 @@
 package framework
 
+// The model interface
 type Model interface {
 	ControllerChild
 }
 
+// The base model struct
 type BaseModel struct {
 }
 
+// Set the parent controller for the model
 func (m *BaseModel) SetParent(c *BaseController) {
 	c.model = m
 }
